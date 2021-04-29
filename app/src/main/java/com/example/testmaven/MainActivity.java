93592442ecf2ee60a80db7d881f2499053c7f782
zz <zhangzhuo1024@163.com>
2021-04-29 22:39:06 +0800

    恢复

--- app/src/main/java/com/example/testmaven/MainActivity.java
+++ app/src/main/java/com/example/testmaven/MainActivity.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.androidutils.AppUtils;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AppUtils.getAppName(this);
     }
 }
