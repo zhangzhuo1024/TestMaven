7360cd0437c896458b9bd7759ad9347a5b4d1401
zz <zhangzhuo1024@163.com>
2021-04-29 23:03:16 +0800

    分别应用两个已经上传的jitpack的依赖

--- app/src/main/java/com/example/testmaven/MainActivity.java
+++ app/src/main/java/com/example/testmaven/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.androidutils.AppUtils;
+import com.quarkboom.androidsoutils.AppSoUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //引用AndroidUtils的
+        AppUtils.getAppName(this);
+
+        //引用AndroidSoUtils的
+        AppSoUtils.getNumber(1, 2);
     }
 }
